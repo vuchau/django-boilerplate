5ba1119045d3979f6131ee842fcf9fde5996327f
Ali Bozorgkhan <alibozorgkhan@gmail.com>
2015-09-15 13:12:01 -0700

    added circleci settings

---
+++ django_boilerplate/settings/circleci.py
@@ -0,0 +1,13 @@
+from .prod import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'circle_test',
+        'USER': 'ubuntu',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
